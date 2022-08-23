a8908a5bf202a91819ccef74dd585816f33e0fcd
tellomaxi <40808669+tellomaxi@users.noreply.github.com>
2022-08-22 22:23:58 -0300

    Code - Security News Feed

---
+++ Security News Feed/newsFeed.py
@@ -0,0 +1,36 @@
+import feedparser
+import webbrowser
+
+
+def main():
+    print("--Security News Website List--")
+    print("0- The HackerNews")
+    print("1- ThreatPost")
+    print("2- NakedSecurity")
+
+    website_list = ("https://feeds.feedburner.com/TheHackersNews", "https://threatpost.com/feed", "https://nakedsecurity.sophos.com/")
+
+    website_input = int(input("Enter website numbre: "))
+
+    NewsFeed = feedparser.parse(website_list[website_input])
+    article_list = []
+    article_link = []
+    for i in range(5):
+        article = NewsFeed.entries[i]
+        titles = article.title
+        link = article.link
+        article_link.append(link)
+        article_list.append(titles)
+
+    article_num = 1
+    for article in article_list:
+        print('[{}] {}'.format(str(article_num),article))
+        article_num+= 1
+    
+    article_link_click = False
+    while not article_link_click:
+        user_click = int(input("Choose the link you want to open(1-5): "))
+        webbrowser.open(article_link[user_click-1])
+        article_link_click=True
+
+main ()
